68752094c1eba1020766db92841b206f9d3812b6
Timothy Pfafman <tim@pfafman.com>
2014-08-27 11:23:18 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Bootstrap 3.2.0
+===============
+
+[Bootstrap 3.2.0](https://github.com/twbs/bootstrap) packaged for [Meteor](https://www.meteor.com) 0.9+
+
+Packaged by [Tim Pfafman](https://github.com/pfafman/meteor-bootstrap-3)
+
+
+### Installation
+
+```
+meteor add pfafman:bootstrap-3
+```
+
+#### Why another package of this?
+
+I needed boostrap 3 on meteor 0.9 and the package there was not working.  Also looks like a fair amount of thinking to get the [less version](https://github.com/simison/bootstrap3-less/) of this to work.
---
+++ package.js
@@ -0,0 +1,34 @@
+Package.describe({
+  summary: "Bootstrap 3 packaged for meteor",
+  version: "3.2.0",
+  git: "https://github.com/pfafman/meteor-bootstrap-3.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
+
+  api.addFiles([
+    'lib/css/bootstrap.css',
+    /*'lib/css/bootstrap-theme.css',*/
+    'lib/js/bootstrap.js',
+    'lib/fonts/glyphicons-halflings-regular.eot',
+    'lib/fonts/glyphicons-halflings-regular.ttf',
+    'lib/fonts/glyphicons-halflings-regular.svg',
+    'lib/fonts/glyphicons-halflings-regular.woff'
+  ], 'client');
+});
+
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('pfafman:bootstrap-3');
+  api.addFiles([
+    'lib/css/bootstrap.css',
+    /*'lib/css/bootstrap-theme.css', */
+    'lib/js/bootstrap.js',
+    'lib/fonts/glyphicons-halflings-regular.eot',
+    'lib/fonts/glyphicons-halflings-regular.ttf',
+    'lib/fonts/glyphicons-halflings-regular.svg',
+    'lib/fonts/glyphicons-halflings-regular.woff'
+  ], 'client');
+});
